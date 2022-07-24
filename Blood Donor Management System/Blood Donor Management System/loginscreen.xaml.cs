4e8d2d0ef3f55f8accd9fb47943cb04506d73a63
bsef19m531 <rashidayesha208@gmail.com>
2022-07-24 15:37:38 +0500

    login page created

---
+++ Blood Donor Management System/Blood Donor Management System/loginscreen.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Blood_Donor_Management_System
+{
+    /// <summary>
+    /// Interaction logic for loginscreen.xaml
+    /// </summary>
+    public partial class loginscreen : Window
+    {
+        public loginscreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
